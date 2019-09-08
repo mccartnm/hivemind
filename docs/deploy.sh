f05d411a59588cfd88a4eb5c1aa22f64a9d1a009
Michael McCartney <mmccartney@stereodllc.com>
2019-09-08 14:06:19 -0400

    Working out the deployment of docs. Test 1

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,1 +1,25 @@
-echo foo_bar
+#!/bin/sh
+
+last_commit_hash=`git log --format=%H -1`
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  pip install --user sphinx
+  cd ../../
+  git clone --depth=50 --branch=master https://github.com/mccartnm/hivemind-docs.git mccartnm/hivemind-docs
+  cd mccartnm/hivemind/docs
+  make github
+  cd ../hivemind-docs
+  git remote rm origin
+  git remote add origin https://user:${GH_TOKEN}@github.com/mccartnm/hivemind-docs.git
+  git add -A
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER - From Hash: $last_commit_hash"
+  git push --quiet origin master
+}
+
+setup_git
+commit_website_files
