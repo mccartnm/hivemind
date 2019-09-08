ecd309ec3af416a5557cb0d108d91e4def54484e
Michael McCartney <mmccartney@stereodllc.com>
2019-09-08 12:39:31 -0400

    Initial setup of the travic ci tools

---
+++ tests/test_hivemind.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+# Get to the right path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import unittest
+
+
+class HiveMindTests(unittest.TestCase):
+    """
+    TODO
+    """
+    def test_foo(self):
+        pass
+
+
+# ----------------------------------------------------------------------------------------------
+# -- Main Function to run tests
+# ----------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+
+    suite.addTests(loader.loadTestsFromTestCase(HiveMindTests))
+    unittest.TextTestRunner(verbosity=2).run(suite)
